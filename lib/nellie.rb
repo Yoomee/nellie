621fc7f6342955ec9a180a000ae368c58c5cd8b3
Greg Woodcock <greg@yoomee.com>
2015-03-05 13:27:27 +0000

    Don't require oauth in nellie.rb

--- lib/nellie.rb
+++ lib/nellie.rb
@@ -4,8 +4,6 @@ require File.expand_path('../nellie/api', __FILE__)
 require File.expand_path('../nellie/client', __FILE__)
 require File.expand_path('../nellie/response', __FILE__)
 
-require 'OAuth2'
-
 module Nellie
   extend Configuration
 
