f1e66671f8b3a704ada11c5028c08daaad073a6f
Greg Woodcock <greg@yoomee.com>
2015-03-05 13:28:09 +0000

    Use the api endpoint from the intialiser rather than hardoding it

--- lib/nellie/client/user.rb
+++ lib/nellie/client/user.rb
@@ -3,7 +3,7 @@ module Nellie
     module Users
 
         def login(username, password)
-          client = OAuth2::Client.new(Nellie.client_id, Nellie.client_secret, :site => "http://localhost:3001")
+          client = OAuth2::Client.new(Nellie.client_id, Nellie.client_secret, :site => Nellie.endpoint)
           return client.password.get_token(username, password).to_hash
         end
 
