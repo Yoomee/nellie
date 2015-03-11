e16405a13239d7ac40f5c07fbd805071cc077262
Greg Woodcock <greg@yoomee.com>
2015-03-11 14:55:59 +0000

    Add method to get list of posts

--- lib/nellie/client/posts.rb
+++ lib/nellie/client/posts.rb
@@ -16,8 +16,21 @@ module Nellie
       #     Nellie.user_posts(1234)
       # @format :json
       # @authenticated true
-      def user_posts(id, *opts)
+      def user_posts(id, opts)
         response = get("users/#{id}/posts", opts)
+        response
+      end
+
+      # Returns a list of posts
+      #
+      # @overload user_posts(id)
+      #   @return [Hash] The requested posts.
+      #   @example Returns a list of posts for the user id of ID 1234
+      #     Nellie.user_posts(1234)
+      # @format :json
+      # @authenticated true
+      def posts(*opts)
+        response = get("posts", opts)
         response
       end
 
