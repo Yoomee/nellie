112871057d91ff3d9f85be6e5de8c28015987a09
Greg Woodcock <greg@yoomee.com>
2015-04-22 11:14:48 +0100

    Add method to authenticate with the api via facebook

--- lib/nellie/client/user.rb
+++ lib/nellie/client/user.rb
@@ -11,6 +11,11 @@ module Nellie
           client = OAuth2::Client.new(Nellie.client_id, Nellie.client_secret, :site => Nellie.endpoint, :raise_errors  => false)
           return client.password.get_token(username, password).to_hash
         end
+
+        def facebook_login(token)
+          client = OAuth2::Client.new(Nellie.client_id, Nellie.client_secret, site: Nellie.endpoint, raise_errors: false)
+          return client.assertion.get_token(hmac_secret: token).to_hash
+        end
       end
 
       def latest_users(opts = {})
