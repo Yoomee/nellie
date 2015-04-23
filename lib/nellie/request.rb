19f4b4295afbea22ae0311e2d17a8b0005b0f93a
Greg Woodcock <greg@yoomee.com>
2015-04-23 13:15:06 +0100

    When authenticated with facebook and the access token expires, use the facebook assertion to generate another one

--- lib/nellie/request.rb
+++ lib/nellie/request.rb
@@ -28,11 +28,27 @@ module Nellie
         puts "Access token not valid"
         puts "Trying to get a new one"
         client = OAuth2::Client.new(Nellie.client_id, Nellie.client_secret, :site => Nellie.endpoint)
-        token = OAuth2::AccessToken.new(client, self.access_token, refresh_token: self.refresh_token)
-        token = token.refresh!
-        store[:access_token] = token.token
-        store[:refresh_token] = token.refresh_token
+        if store[:refresh_token].present?
+          #The user logged in with a user name and password, get a new token using the refresh token
+          token = refresh_token_with_refresh_token(client, store[:access_token], store[:refresh_token])
+          store[:access_token] = token.token
+          store[:refresh_token] = token.refresh_token
+        else
+          #The user logged in with facebook, get a new token with the facebook token
+          token = refresh_token_with_facebook(client, store[:facebook_token])
+          store[:access_token] = token[:access_token]
+        end
       end
+    end
+
+    def refresh_token_with_refresh_token(client, access_token, refresh_token)
+      token = OAuth2::AccessToken.new(client, access_token, refresh_token)
+      client.assertion.get_token(hmac_secret: token).to_hash
+      token.refresh!
+    end
+
+    def refresh_token_with_facebook(client, facebook_token)
+      client.assertion.get_token(hmac_secret: facebook_token).to_hash
     end
 
     # Perform an HTTP request
