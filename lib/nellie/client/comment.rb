aa5f6766d5d9319b5c4cd2865af4f4bd37272816
Tom Kitchin <thomas.kitchin@yoomee.com>
2015-09-24 16:10:38 +0100

    Include opts in comments calls

--- lib/nellie/client/comment.rb
+++ lib/nellie/client/comment.rb
@@ -7,28 +7,23 @@ module Nellie
       end
 
       def create_comment(post_id, text, moderator_id, opts = {})
-        response = post("posts/#{post_id}/comments", text: text, moderator_id: moderator_id)
-        response
+        post("posts/#{post_id}/comments", text: text, moderator_id: moderator_id, opts)
       end
 
       def update_comment(id, text, opts = {})
-        response = put("comments/#{id}", text: text)
-        response
+        put("comments/#{id}", text: text, opts)
       end
 
       def like_comment(id, type, opts = {})
-        response = post("comments/#{id}/likes", type: type)
-        response
+        post("comments/#{id}/likes", type: type, opts)
       end
 
       def unlike_comment(id, type, opts = {})
-        response = delete("comments/#{id}/likes", type: type)
-        response
+        delete("comments/#{id}/likes", type: type, opts)
       end
 
       def comment_likes(id, type, opts = {})
-        response = get("comments/#{id}/likes", type: type)
-        response
+        get("comments/#{id}/likes", type: type, opts)
       end
 
     end
