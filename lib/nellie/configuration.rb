d082cb799d90c9ebff66eab72fcd2e13cc43462e
Greg Woodcock <greg@yoomee.com>
2016-02-18 16:56:52 +0000

    Change the user agent to Nelli

--- lib/nellie/configuration.rb
+++ lib/nellie/configuration.rb
@@ -72,7 +72,7 @@ module Nellie
     DEFAULT_NO_RESPONSE_WRAPPER = false
 
     # The user agent that will be sent to the API endpoint if none is set
-    DEFAULT_USER_AGENT = "Elefriends Ruby Gem #{Nellie::VERSION}".freeze
+    DEFAULT_USER_AGENT = "Nellie".freeze
 
     STORE = {}
 
